b63e5a4fa8b706120d63c441ed01608f123644e1
ritian <393917309@qq.com>
2019-04-22 22:47:31 +0800

    😊建造者模式

---
+++ src/main/java/com/ritian/designpattern/builder/BuilderTest.java
@@ -0,0 +1,34 @@
+package com.ritian.designpattern.builder;
+
+/**
+ * @author ritian.Zhang
+ * @date 2019/04/22
+ **/
+public class BuilderTest {
+
+
+    public static void main(String[] args) {
+        //builder模式 director
+        Builder builder = new CharacterBuilder();
+        Director director = new Director(builder);
+        Character character = director.createCharacter("基佬", "臭脸", "比基尼");
+        System.out.println(character.showMsg());
+
+
+        //非builder模式
+        Computer computer = new Computer("cpu", "screen", "memory", "mainboard");
+
+        //builder模式 省略director
+        NewComputer newComputer = new NewComputer.Builder()
+                .cpu("cpu1")
+                .screen("screen")
+                .memory("memory")
+                .mainboard("mainboard")
+                .build();
+        System.out.println(computer.toString());
+        System.out.println(newComputer.toString());
+
+
+
+    }
+}
