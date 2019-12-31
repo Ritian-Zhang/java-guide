402407f480f28a5f130c34b21d0fcbb9f81ec5c0
ritian <393917309@qq.com>
2019-12-31 10:55:49 +0800

    Add 位运算☁

---
+++ src/main/java/com/ritian/operation/OperationDemo.java
@@ -0,0 +1,34 @@
+package com.ritian.operation;
+
+/**
+ * java中常忽略的运算符
+ * <p>
+ * << . >>   ||.|      && . &
+ *
+ * @author ritian
+ * @since 2019/12/10 18:28
+ **/
+public class OperationDemo {
+
+    /**
+     * <pre>
+     *      "|" 位运算(不短路),表示 两个位只要有一个为1，那么结果就是1，否则就为0
+     *       例如 ： 2 | 3  表示2和3的二进制位运算
+     *             00000010
+     *             00000011
+     *         --------------
+     *             00000011
+     *
+     *
+     *  </pre>
+     */
+    public static int ctlOf(int rs, int wc) {
+        return rs | wc;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(ctlOf(2,3));
+    }
+
+}
