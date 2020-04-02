37d2bb5fc630edbc57c6a7941bbbb220459f4728
ritian <393917309@qq.com>
2020-04-02 22:03:11 +0800

    java并发之死锁

---
+++ src/main/java/com/ritian/lock/DeadLockDemo.java
@@ -0,0 +1,52 @@
+package com.ritian.lock;
+
+/**
+ * 死锁
+ *
+ * @author ritian
+ * @since 2020/4/2 21:43
+ **/
+public class DeadLockDemo {
+
+    private static String A = "A";
+    private static String B = "B";
+
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    /**
+     * 演示死锁的场景
+     * 可以dump线程查看到底是哪个线程出现了问题。(可以看到 线程的状态是 BLOCKED)
+     * 命令： jstack pid > dumpfile
+     */
+    private void deadLock() {
+        Thread t1 = new Thread(() -> {
+            synchronized (A) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (B) {
+                    System.out.println("1");
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            synchronized (B) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (A) {
+                    System.out.println("2");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
--- src/main/java/com/ritian/lock/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- *
- * @author ritian
- * @since 2019/7/17 19:26
- **/
-package com.ritian.lock;
