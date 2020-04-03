28719c6e894527c74687a8f8a124cf98dc889ffc
ritian <393917309@qq.com>
2020-04-03 23:01:38 +0800

    java并发之synchronized

--- src/main/java/com/ritian/thread/jmm/SynchronizedDemo.java
+++ src/main/java/com/ritian/thread/jmm/SynchronizedDemo.java
@@ -1,10 +1,49 @@
 package com.ritian.thread.jmm;
 
+import cn.hutool.core.thread.ThreadUtil;
+
 /**
- * synchronized的实现原理与应用 TODO
+ * synchronized的实现原理与应用
  *
  * @author ritian
  * @since 2020/4/2 22:59
  **/
 public class SynchronizedDemo {
+
+
+    static {
+        //锁住的是Class对象
+        synchronized (SynchronizedDemo.class) {
+            System.out.println("锁住的是这个类的所有对象");
+        }
+    }
+
+    public static void main(String[] args) {
+        SynchronizedDemo demo1 = new SynchronizedDemo();
+        SynchronizedDemo demo2 = new SynchronizedDemo();
+
+        Thread thread1 = new Thread(() -> {
+            demo1.test1();
+        });
+        Thread thread2 = new Thread(() -> {
+            demo1.test1();
+        });
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+    private void test1() {
+        synchronized (this) {
+            ThreadUtil.sleep(1000);
+            System.out.println("锁住的是调用这个代码块的对象");
+        }
+    }
+
+    private synchronized void test2() {
+        System.out.println("锁住的是调用这个方法的对象");
+    }
+
+
 }
