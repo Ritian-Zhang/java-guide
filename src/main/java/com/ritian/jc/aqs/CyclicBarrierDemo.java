37d54efac397b8d5e0c0338dd69021f051fdcbe5
ritian <393917309@qq.com>
2020-05-05 17:34:49 +0800

    循环栅栏-CyclicBarrier

---
+++ src/main/java/com/ritian/jc/aqs/CyclicBarrierDemo.java
@@ -0,0 +1,38 @@
+package com.ritian.jc.aqs;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 循环栅栏--->实现让一组线程等待至某个状态之后再全部同时执行
+ * <p>CyclicBarrier在使用一次后，下面依然有效，可以继续当做计数器使用，这是与CountDownLatch的区别之一。
+ *
+ * @author ritian
+ * @since 2020/5/3 23:09
+ **/
+public class CyclicBarrierDemo {
+
+    private static CyclicBarrier barrier = new CyclicBarrier(6, () -> System.out.println("所有运动员入场，裁判员一声令下！！！！"));
+
+    public static void main(String[] args) {
+        System.out.println("运动员准备进场，全场欢呼。。。。。");
+        ExecutorService executorService = Executors.newFixedThreadPool(12);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " 运动员,进场");
+                    barrier.await();
+                    System.out.println(barrier.getNumberWaiting());
+                    System.out.println(Thread.currentThread().getName() + " 运动员,出发");
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+}
